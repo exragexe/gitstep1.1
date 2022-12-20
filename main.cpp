87e8743222e7093d66d9d4691508a0715ce91bfa
yaros <yaroslavdisiak@gmail.com>
2022-12-21 00:20:51 +0200

    ex1done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
 
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    system("color 02");
+    cout << "Name"<<"\t   "": "<< "\"The war and the peace\""<< endl;
+    cout << "Avtor" << "\t   "": " << "L.N. Tolstoj" << endl;
+    cout << "Izdatelstvo" << ": " << "Piter" << endl;
+    cout << "Pages" << "\t   "": " << "500." << endl;
+    return 0;
+}
