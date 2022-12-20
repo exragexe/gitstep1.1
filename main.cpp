620771ceabbc9076eeee6cc35075b7ed2a3833b6
yaros <yaroslavdisiak@gmail.com>
2022-12-21 00:47:14 +0200

    ex3done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
 
+
+#include <iostream>
+using namespace std;
+int main() {
+    system("color 05");
+    cout << (char)201<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)187;
+    cout<<"\n"<<endl;
+    cout <<(char)186<<"            Pory Roku"<<"                             "<<(char)186;
+    cout<<"\n"<<endl;
+    cout<<(char)204<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)185;
+    cout<<"\n"<<endl;
+    cout <<(char)186<<"   Zyma   "<<(char)186<<"   Vesna    "<<(char)186<<"      Lito   "<<(char)186<<"   Osin     "<<(char)186;
+    cout <<"\n"<<endl;
+    cout << (char)200<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+    cout<< (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)188;
+
+    return 0;
+}
