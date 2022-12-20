2d9ece7b3d1499cdec807d0a50f7bd62d58ffacc
yaros <yaroslavdisiak@gmail.com>
2022-12-21 00:23:49 +0200

    ex2done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
 
+
+#include <iostream>
+using namespace std;
+int main() {
+    system("color 05");
+    cout << "\"Kraj Lukomor`a dub zelenyj,\n\aI zolotyj lancyuh na nim:\n\aShodnya,";
+    cout<<" schonochi kit uchenyj\n\aNa lancyuhu kruzhl`aje tim\"";
+    return 0;
+}
